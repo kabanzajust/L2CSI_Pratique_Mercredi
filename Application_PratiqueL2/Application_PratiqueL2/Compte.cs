6f42c866e229f63e4d2c7755c6c4a6b1b387bb67
l2csi2022 <iscgomal2csi@gmail.com>
2022-05-04 16:07:10 +0200

    Création Classe

---
+++ Application_PratiqueL2/Application_PratiqueL2/Compte.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_PratiqueL2
+{
+    class Compte
+    {
+        private int idcompte;
+
+        public int Idcompte
+        {
+            get { return idcompte; }
+            set { idcompte = value; }
+        }
+    }
+}
